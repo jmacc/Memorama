bb29145dc780153869af74932020f5190e2fad04
jmacc <j2macc@gmail.com>
2022-10-29 19:53:48 -0500

    imagenes aletorias agregadas

---
+++ app/src/main/java/com/tianalytics/memorama/Imagen.kt
@@ -0,0 +1,7 @@
+package com.tianalytics.memorama
+
+class Imagen {
+	var id_vector = 0
+	var posicion = 0
+	var asignada = false
+}
--- app/src/main/java/com/tianalytics/memorama/MainActivity.kt
+++ app/src/main/java/com/tianalytics/memorama/MainActivity.kt
@@ -2,13 +2,94 @@ package com.tianalytics.memorama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
+	var listaImageViewRotas = ArrayList<ImageView>()
+	var listaImageView = ArrayList<ImageView>()
+	var listaPosiciones = ArrayList<Int>()
+	var listaClaseImagen = ArrayList<Imagen>()
 
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		listaImageView.add(img0)
+		listaImageView.add(img1)
+		listaImageView.add(img2)
+		listaImageView.add(img3)
+		listaImageView.add(img4)
+		listaImageView.add(img5)
+		listaImageView.add(img6)
+		listaImageView.add(img7)
+		listaImageView.add(img8)
+
+		listaImageViewRotas.add(img0Rota)
+		listaImageViewRotas.add(img1Rota)
+		listaImageViewRotas.add(img2Rota)
+		listaImageViewRotas.add(img3Rota)
+		listaImageViewRotas.add(img4Rota)
+		listaImageViewRotas.add(img5Rota)
+		listaImageViewRotas.add(img6Rota)
+		listaImageViewRotas.add(img7Rota)
+		listaImageViewRotas.add(img8Rota)
+
+		ocultarImagenes()
+		cargarPosicionesRandom()
+
+	}
+//mostrar el inicio del juego
+
+	fun ocultarImagenes(){
+		for (i in 0..7){
+			listaImageViewRotas[i].visibility = View.VISIBLE
+			listaImageView[i].visibility = View.INVISIBLE
+		}
+	}
+
+	/**
+	  * Mostrar arreglo Ordenado
+	 */
+		fun cargarPosicionesRandom(){
+			listaPosiciones.add(0)
+			listaPosiciones.add(2)
+			listaPosiciones.add(3)
+			listaPosiciones.add(4)
+			listaPosiciones.add(5)
+			listaPosiciones.add(6)
+			listaPosiciones.add(7)
+			Log.d("mensaje","original $listaPosiciones")
+		/**
+		 * Mostrar Arreglo desordenado
+		 */
+		Collection.shuffle(listaPosiciones)
+		Log.d("mensaje","original $listaPosiciones")
+
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+
+		/**
+		 * Cada imagen debe de tener dos posiciones random
+		 */
+		for(i in 0..7)
+		var posicion = listaPosiciones[i]
+			var id_vector = 0
+
+		when(i){
+			0,1 -> { id_vector = R.drawable.ic_carriola}
+			2,3 -> {id_vector = R.drawable.ic_deck}
+			4,5 -> {id_vector = R.drawable.ic_station}
+			6,7 -> {id_vector =R.drawable.ic_transportation}
+		}
+
 	}
 }
