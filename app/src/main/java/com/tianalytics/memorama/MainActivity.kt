1cbfc7b5cc7760587a3c6e12542decd9cbdc5386
jmacc <j2macc@gmail.com>
2022-10-29 23:06:37 -0500

    correcion de errores

--- app/src/main/java/com/tianalytics/memorama/MainActivity.kt
+++ app/src/main/java/com/tianalytics/memorama/MainActivity.kt
@@ -5,70 +5,92 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import com.tianalytics.memorama.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+	// declaramos ViewBinding
+	private lateinit var binding: ActivityMainBinding
 
-	var listaImageViewRotas = ArrayList<ImageView>()
-	var listaImageView = ArrayList<ImageView>()
-	var listaPosiciones = ArrayList<Int>()
-	var listaClaseImagen = ArrayList<Imagen>()
+	private var listaImageViewRotas = ArrayList<ImageView>()
+	private var listaImageView = ArrayList<ImageView>()
+	private var listaPosiciones = ArrayList<Int>()
+	private var listaClaseImagen = ArrayList<Imagen>()
 
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		//inicializamos viewBinding
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
-		listaImageView.add(img0)
-		listaImageView.add(img1)
-		listaImageView.add(img2)
-		listaImageView.add(img3)
-		listaImageView.add(img4)
-		listaImageView.add(img5)
-		listaImageView.add(img6)
-		listaImageView.add(img7)
-		listaImageView.add(img8)
+		listaImageView.add(binding.img0)
+		listaImageView.add(binding.img1)
+		listaImageView.add(binding.img2)
+		listaImageView.add(binding.img3)
+		listaImageView.add(binding.img4)
+		listaImageView.add(binding.img5)
+		listaImageView.add(binding.img6)
+		listaImageView.add(binding.img7)
 
-		listaImageViewRotas.add(img0Rota)
-		listaImageViewRotas.add(img1Rota)
-		listaImageViewRotas.add(img2Rota)
-		listaImageViewRotas.add(img3Rota)
-		listaImageViewRotas.add(img4Rota)
-		listaImageViewRotas.add(img5Rota)
-		listaImageViewRotas.add(img6Rota)
-		listaImageViewRotas.add(img7Rota)
-		listaImageViewRotas.add(img8Rota)
+		listaImageViewRotas.add(binding.img0Rota)
+		listaImageViewRotas.add(binding.img1Rota)
+		listaImageViewRotas.add(binding.img2Rota)
+		listaImageViewRotas.add(binding.img3Rota)
+		listaImageViewRotas.add(binding.img4Rota)
+		listaImageViewRotas.add(binding.img5Rota)
+		listaImageViewRotas.add(binding.img6Rota)
+		listaImageViewRotas.add(binding.img7Rota)
+
 
 		ocultarImagenes()
 		cargarPosicionesRandom()
+		/**
+		 * Nos permite visualizar las imagenes en forma aleatoria
+		 */
+		//mostrarImagenes()
 
 	}
 //mostrar el inicio del juego
 
-	fun ocultarImagenes(){
-		for (i in 0..7){
-			listaImageViewRotas[i].visibility = View.VISIBLE
+	private fun ocultarImagenes() {
+		for (i in 0..7) {
+			listaImageViewRotas[i].visibility = View.INVISIBLE
 			listaImageView[i].visibility = View.INVISIBLE
 		}
 	}
 
+	private fun mostrarImagenes() {
+		for (i in 0..7) {
+			listaImageViewRotas[i].visibility = View.INVISIBLE
+			listaImageView[i].visibility = View.VISIBLE
+		}
+	}
+
 	/**
-	  * Mostrar arreglo Ordenado
+	 * Mostrar arreglo Ordenado
 	 */
-		fun cargarPosicionesRandom(){
-			listaPosiciones.add(0)
-			listaPosiciones.add(2)
-			listaPosiciones.add(3)
-			listaPosiciones.add(4)
-			listaPosiciones.add(5)
-			listaPosiciones.add(6)
-			listaPosiciones.add(7)
-			Log.d("mensaje","original $listaPosiciones")
+	private fun cargarPosicionesRandom() {
+		listaPosiciones.add(0)
+		listaPosiciones.add(1)
+		listaPosiciones.add(2)
+		listaPosiciones.add(3)
+		listaPosiciones.add(4)
+		listaPosiciones.add(5)
+		listaPosiciones.add(6)
+		listaPosiciones.add(7)
+		Log.d("mensaje", "original $listaPosiciones")
+
 		/**
 		 * Mostrar Arreglo desordenado
 		 */
-		Collection.shuffle(listaPosiciones)
-		Log.d("mensaje","original $listaPosiciones")
 
+		Collections.shuffle(listaPosiciones)
+
+		//shuffled(listaPosiciones)
+		Log.d("mensaje", "desordenado $listaPosiciones")
+		listaClaseImagen.add(Imagen())
 		listaClaseImagen.add(Imagen())
 		listaClaseImagen.add(Imagen())
 		listaClaseImagen.add(Imagen())
@@ -78,18 +100,36 @@ class MainActivity : AppCompatActivity() {
 		listaClaseImagen.add(Imagen())
 
 		/**
-		 * Cada imagen debe de tener dos posiciones random
+		 * each image should have two random positions
 		 */
-		for(i in 0..7)
-		var posicion = listaPosiciones[i]
-			var id_vector = 0
+		for (i in 0..7) {
+			val posiciones = listaPosiciones[i]
+			var idDrawable = 0
 
-		when(i){
-			0,1 -> { id_vector = R.drawable.ic_carriola}
-			2,3 -> {id_vector = R.drawable.ic_deck}
-			4,5 -> {id_vector = R.drawable.ic_station}
-			6,7 -> {id_vector =R.drawable.ic_transportation}
-		}
+			when (i) {
+				0, 1 -> {
+					idDrawable = R.drawable.ic_carriola
+				}
+				2, 3 -> {
+					idDrawable = R.drawable.ic_deck
+				}
+				4, 5 -> {
+					idDrawable = R.drawable.ic_station
+				}
+				6, 7 -> {
+					idDrawable = R.drawable.ic_transportation
+				}
+			}
+		/**
+		 * agregar imagenes al view
+		 */
+		listaImageView[posiciones].setImageResource(idDrawable)
+		val objetoImagen = Imagen().apply { this.id_vector = idDrawable }
 
+		/**
+		 * We place each image in its position
+		 */
+		listaClaseImagen[posiciones] = objetoImagen
 	}
+}
 }
