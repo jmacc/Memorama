66e984489a348a53f61e0ab199e20f4cc736a0ae
jmacc <j2macc@gmail.com>
2022-11-05 18:21:30 -0600

    Nuevos Iconos

--- app/src/main/java/com/tianalytics/memorama/MainActivity.kt
+++ app/src/main/java/com/tianalytics/memorama/MainActivity.kt
@@ -9,9 +9,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import com.tianalytics.memorama.databinding.ActivityMainBinding
-import java.sql.Time
 import java.util.*
-import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -41,6 +39,10 @@ class MainActivity : AppCompatActivity() {
 		listaImageView.add(binding.img5)
 		listaImageView.add(binding.img6)
 		listaImageView.add(binding.img7)
+		listaImageView.add(binding.img8)
+		listaImageView.add(binding.img9)
+		listaImageView.add(binding.img10)
+		listaImageView.add(binding.img11)
 
 		listaImageViewRotas.add(binding.img0Rota)
 		listaImageViewRotas.add(binding.img1Rota)
@@ -50,6 +52,10 @@ class MainActivity : AppCompatActivity() {
 		listaImageViewRotas.add(binding.img5Rota)
 		listaImageViewRotas.add(binding.img6Rota)
 		listaImageViewRotas.add(binding.img7Rota)
+		listaImageViewRotas.add(binding.img8Rota)
+		listaImageViewRotas.add(binding.img9Rota)
+		listaImageViewRotas.add(binding.img10Rota)
+		listaImageViewRotas.add(binding.img11Rota)
 
 
 		ocultarImagenes()
@@ -90,6 +96,10 @@ class MainActivity : AppCompatActivity() {
 		listaPosiciones.add(5)
 		listaPosiciones.add(6)
 		listaPosiciones.add(7)
+		listaPosiciones.add(8)
+		listaPosiciones.add(9)
+		listaPosiciones.add(10)
+		listaPosiciones.add(11)
 		Log.d("mensaje", "original $listaPosiciones")
 
 		/**
@@ -108,26 +118,36 @@ class MainActivity : AppCompatActivity() {
 		listaClaseImagen.add(Imagen())
 		listaClaseImagen.add(Imagen())
 		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
+		listaClaseImagen.add(Imagen())
 
 		/**
 		 * each image should have two random positions
 		 */
-		for (i in 0..7) {
+		for (i in 0..11) {
 			val posiciones = listaPosiciones[i]
 			var idDrawable = 0
 
 			when (i) {
 				0, 1 -> {
-					idDrawable = R.drawable.ic_carriola
+					idDrawable = R.drawable.Bala
 				}
 				2, 3 -> {
-					idDrawable = R.drawable.ic_deck
+					idDrawable = R.drawable.Fantasma
 				}
 				4, 5 -> {
-					idDrawable = R.drawable.ic_station
+					idDrawable = R.drawable.Hongo
 				}
 				6, 7 -> {
-					idDrawable = R.drawable.ic_transportation
+					idDrawable = R.drawable.LuigiMedalla
+				}
+				8, 9 -> {
+					idDrawable = R.drawable.Princesa
+				}
+				10, 11 -> {
+					idDrawable = R.drawable.TortugaMala
 				}
 			}
 		/**
@@ -144,8 +164,8 @@ class MainActivity : AppCompatActivity() {
 }
 
 	 fun procesarImagen(imagenView: View){
-		var posicionSeleccionada:Int = imagenView.tag.toString().toInt()
-		var id:String =imagenView.resources.getResourceEntryName(imagenView.id)
+		val posicionSeleccionada:Int = imagenView.tag.toString().toInt()
+		val id:String =imagenView.resources.getResourceEntryName(imagenView.id)
 		/**
 		 * Show images on click
 		 */
@@ -175,6 +195,18 @@ class MainActivity : AppCompatActivity() {
 				"img7Rota" -> {
 					mostrarOcultarImagen(posicionSeleccionada,true)
 				}
+				"img8Rota" -> {
+					mostrarOcultarImagen(posicionSeleccionada,true)
+				}
+				"img9Rota" -> {
+					mostrarOcultarImagen(posicionSeleccionada,true)
+				}
+				"img10Rota" -> {
+					mostrarOcultarImagen(posicionSeleccionada,true)
+				}
+				"img11Rota" -> {
+					mostrarOcultarImagen(posicionSeleccionada,true)
+				}
 				"img0" -> {
 					mostrarOcultarImagen(posicionSeleccionada,false)
 				}
@@ -197,6 +229,18 @@ class MainActivity : AppCompatActivity() {
 					mostrarOcultarImagen(posicionSeleccionada,false)
 				}
 				"img7" -> {
+					mostrarOcultarImagen(posicionSeleccionada,false)
+				}
+				"img8" -> {
+					mostrarOcultarImagen(posicionSeleccionada,false)
+				}
+				"img9" -> {
+					mostrarOcultarImagen(posicionSeleccionada,false)
+				}
+				"img10" -> {
+					mostrarOcultarImagen(posicionSeleccionada,false)
+				}
+				"img11" -> {
 					mostrarOcultarImagen(posicionSeleccionada,false)
 				}
 			}
@@ -248,7 +292,10 @@ class MainActivity : AppCompatActivity() {
 
 	private fun reiniciarJuego(view: View){
 		var intent = getIntent()
-		finish()
 		startActivity(intent)
+	}
+	private fun finalizarJuego(view: View){
+		var intent = getIntent()
+		finish()
 	}
 }
